34375635ccd05bfcf9655092d78553252b5ef2dd
Nicolas Dumoulin <nicolas.dumoulin@irstea.fr>
2013-06-08 17:57:11 +0200

    all moves possibles computed

--- computeTransitions.py
+++ computeTransitions.py
@@ -1,46 +1,3 @@
- 
-def buildMatrix(size):
-  return [[i*size+j for j in range(size)] for i in range(size)]
-
-def ijFromIndex(size, index):
-  return [index/size,index%size]
-
-def distance(iStart,jStart,iEnd,jEnd):
-  return abs(iEnd-iStart)+abs(jEnd-jStart)
-
-def destinations(size,iStart,jStart,length,exact=True):
-  destinations=[]
-  for i in range(size):
-    for j in range(size):
-      if exact:
-        if distance(iStart,jStart,i,j)==length:
-          destinations.append([i,j])
-      elif distance(iStart,jStart,i,j)<=length:
-        destinations.append([i,j])
-  return destinations
-
-def destinationsIndexes(size,iStart,jStart,length,exact=True):
-  data=[]
-  matrix=buildMatrix(size)
-  for d in destinations(size,iStart,jStart,length,exact):
-    data.append(matrix[d[0]][d[1]])
-  return data
-
-def computeTransitions():
-  size = 4
-  matrix = buildMatrix(size)
-  for i in range(size*size):
-    buff = "{"
-    for j in range(1,7):
-      ij = ijFromIndex(size,i)
-      end = ""
-      if not j==6: end=", "
-      buff += str(j)+":"+str(destinationsIndexes(size,ij[0],ij[1],j,True))+end
-    buff += "}"
-    if not i == size*size-1:
-      buff += ','
-    print(buff)
-
 import numpy as np
 def destinations(position=2,maxDepth=2,size=4) :
   """
@@ -76,4 +33,7 @@ def destinations(position=2,maxDepth=2,size=4) :
     starts = nextStarts
     depth += 1
   return destinations
+
+if __name__=='__main__':
+  print(str([destinations(position,maxDepth=6,size=4) for position in range(16)]))
     
